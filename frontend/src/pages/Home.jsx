1b51998f2fde6c15e7f42464ba3f511dc5a46652
Shamiska <e21077@eng.pdn.ac.lk>
2025-10-15 10:57:41 +0530

    Initial Commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "../components/Header.jsx"
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
+import TopDoctors from '../components/TopDoctors.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+    
+    </div>
+  )
+}
+
+export default Home
