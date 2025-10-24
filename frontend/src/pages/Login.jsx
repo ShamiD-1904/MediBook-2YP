b2675c96ef8f1b8384f1c749ff674c5dad4ee19b
Shamiska <e21077@eng.pdn.ac.lk>
2025-10-24 07:25:01 +0530

    login / create account pages frontend done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState("Sign Up")
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+  }
   return (
-    <div>
+    <form className='min-h-[80vh] flex items-center'>
+
+      <div className='flex flex-col gap-3 m-auto p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zink-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'sign up' ? "Sign Up" : "log in "}to book appointment</p>
+        {
+          state === "Sign Up" && 
+          <div className='w-full '>
+          <p>Full Name</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e) => setName(e.target.name)} value={name} required />
+        </div>
+
+
+        }
+        
+
+        <div className='w-full '>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e) => setEmail(e.target.name)} value={email} required />
+        </div>
+
+        <div className='w-full '>
+          <p>Full Name</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e) => setPassword(e.target.name)} value={email} required />
+        </div>
+
+        <button className='bg-primary w-full text-white py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === "Sign Up" ? 
+          <p>Already have an account? <span className='text-primary underline cursor-pointer' onClick={() => setState("Login")}>Login here</span></p>
+          : <p>Create a new account? <span onClick={() => setState("Sign Up")} className='text-primary underline cursor-pointer' >Click here</span></p>
+        }
+      </div>
       
-    </div>
+    </form>
   )
 }
 
